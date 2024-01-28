7947317102cc7e7d2cdeccec13b4d061ab530d67
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-28 23:35:07 +0000

    added component

---
+++ components/contentProcessor.js
@@ -0,0 +1,21 @@
+import useSWR from 'swr';
+
+// Custom fetcher to handle POST requests
+const fetcher = async (url, data) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the data.');
+    error.info = await response.json();
+    error.status = response.status;
+    throw error;
+  }
+
+  return response.json();
+};
