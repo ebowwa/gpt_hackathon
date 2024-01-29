fc2ff6c96700586285cef6d1917284f12aaaa6ce
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-29 03:48:54 +0000

    removed agentops llm inference paued

--- api/_backend/inference/client.py
+++ api/_backend/inference/client.py
@@ -1,8 +1,4 @@
-import openai  # Make sure openai is imported before instantiating an AgentOps client.
-import agentops
-
-# Instantiate AgentOps client
-ao_client = agentops.Client('I37539dff-cf9e-4dce-8ca7-9a453fb5dce3>')
+import openai
 
 # OpenAI client class
 class OpenAIClient:
@@ -12,7 +8,6 @@ class OpenAIClient:
             base_url=base_url
         )
 
-    @ao_client.record_action('get_chat_response')
     def get_chat_response(self, message):
         response = self.client.chat.completions.create(model="ollama/mistral", messages=[
             {
@@ -22,12 +17,8 @@ class OpenAIClient:
         ])
         return response
 
-# End of program
-ao_client.end_session('Success')
-
 if __name__ == "__main__":
     # Example usage
     client = OpenAIClient(api_key="null", base_url="https://dev-hub.agentartificial.com")
     response = client.get_chat_response("you are a short clip narrator. you respond in a style that starts and ends in a loop. you attract.. rather manipulatively attract viewers attention. you do not reply in short form")
     print(response)
-  
