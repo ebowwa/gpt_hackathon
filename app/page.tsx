9ac6e99226008a69492cc3d8c0a3e80d1cd0ba3b
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-28 16:34:59 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import { ContentProcessor } from '../../components/ContentProcessor'; // Adjust the path as needed
+import { ContentProcessor } from '../../components/contentProcessor'; // Adjust the path as needed
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
